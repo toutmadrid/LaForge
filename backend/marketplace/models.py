57bf554193c90e84e26a274902729e45fb77d8e3
toutmadrid <max.devulpillieres@gmail.com>
2025-03-10 19:38:48 +0100

    suite

--- backend/marketplace/models.py
+++ backend/marketplace/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from users.models import User
 
 # Modèle Offre (capacité industrielle)
 class Offer(models.Model):
